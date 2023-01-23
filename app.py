051d68e7b0c017f6d78af1898a3b045f1a279269
Aran Smithson <aransmithson@gmail.com>
2023-01-23 20:01:29 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from threading import Thread
 from flask import Flask
 import bot as bot
 
+#set up basic flask app
 app = Flask(__name__)
 
-#t = Thread(target=bot.run)
-#t.start()
+t = Thread(target=bot.run)
+t.start()
 
 @app.route('/')
 def index():
